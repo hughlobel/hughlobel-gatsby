7399232900c42855238a5f62ec5dbeccb1ec9d28
Hugh Lobel <hughlobel@users.noreply.github.com>
2020-09-02 17:19:25 -0500

    tweaking to improve home page issues

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -8,13 +8,13 @@ import VideoBG from '../components/VideoBG'
 import config from '../config/config'
 import projects from '../config/projects'
 
-let fp1 = projects.find(project => project.title === 'MSDP 2');
+let fp1 = projects.find(project => project.title === config.featuredProject1);
 let sectionStyle = {
   backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url('/img/" + fp1.image + "')",
   backgroundSize: "cover"
 }
 // Once grabbed, setup CSS for featured Section based on preview images
-let fp2 = projects.find(project => project.title === 'replMax');
+let fp2 = projects.find(project => project.title === config.featuredProject2);
 let sectionStyle2 = {
   backgroundImage: "linear-gradient(rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0.8)), url('/img/" + fp2.image + "')",
   backgroundSize: "cover"
@@ -23,46 +23,43 @@ let sectionStyle2 = {
 export const IndexPageTemplate = ({
   tagline,
   tagImage,
-  featuredProject1,
-  featuredProject2,
   manifestoSection,
-  bioSection,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
+  bioSection
 }) => (
   <div class="homePage">
     <VideoBG></VideoBG>
     <section class="titleSection">
       <p class="videoInfo"><Fade top>This, and all videos on this site, are captured in MSDP: a multimedia platform created by Hugh Lobel</Fade></p>
       <div className='container titleContent'>
-        <div>
-          <h2>Hugh Lobel is a Full-Stack Web / Music Software Developer and Educator, Building and Sharing Creative Experiences for a Connected World.</h2>
-        </div>
-        <div className="titleImage">
-          <img src='img/hugh_talking.jpg' alt="Hugh Lobel speaking in the Atlas Building at CU Boulder"/>
-        </div>
+        <Fade top>
+          <div>
+            <h2>{tagline}</h2>
+          </div>
+        </Fade>
+        <Fade clear>
+          <div className="titleImage">
+            <img src={`img/${tagImage}`} alt="Hugh Lobel speaking in the Atlas Building at CU Boulder"/>
+          </div>
+        </Fade>
       </div>
     </section>
     <section className="fullScreen homeScreen">
-      <Fade>
+      <Fade clear>
       <div className="fullRow">
         <div className="twoColumns featuredProjects">
           <div class="featuredTitle"><h2>Featured Projects</h2></div>
           <a className="featuredOne perfectCenter" style={ sectionStyle }>
             <Link to={`/projects/${fp1.title}`} className="perfectCenter">
-                <Fade><h3 className="white">{fp1.title}</h3><br /></Fade> 
-                <Fade><p className="white category">{fp1.category}</p><br /></Fade> 
-                <Fade><p className="white">{fp1.excerpt}</p></Fade>
+                <Fade top><h3 className="white">{fp1.title}</h3><br /></Fade> 
+                <Fade clear><p className="white category">{fp1.category}</p><br /></Fade> 
+                <Fade clear><p className="white">{fp1.excerpt}</p></Fade>
             </Link>
           </a>
           <a className="featuredTwo perfectCenter" style={ sectionStyle2 }>
             <Link to={`/projects/${fp2.title}`} className="perfectCenter">
-              <Fade><h3>{fp2.title}</h3><br /></Fade> 
-              <Fade><p className="category">{fp2.category}</p><br /></Fade> 
-              <Fade><p>{fp2.excerpt}</p></Fade> 
+              <Fade top><h3>{fp2.title}</h3><br /></Fade> 
+              <Fade clear><p className="category">{fp2.category}</p><br /></Fade> 
+              <Fade clear><p>{fp2.excerpt}</p></Fade> 
             </Link>
           </a>
         </div>
