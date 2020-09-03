94f437b48246e0a85652b0b4610608cf19d90c7c
Hugh Lobel <hughlobel@users.noreply.github.com>
2020-09-03 11:24:22 -0500

    testing styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Hugh Lobel. Developer, Educator',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'The Official Website of Hugh Lobel'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -67,12 +67,11 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: `gatsby-plugin-styled-components`,
       options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
+        // Add any options here
       },
-    }, // must be after other CSS plugins
+    },
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
 }
