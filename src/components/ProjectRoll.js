dd9c88749f3cb53b433cff2203c30352afc2a96b
Hugh Lobel <hughlobel@users.noreply.github.com>
2020-09-03 10:16:10 -0500

    adding styled components plugin to gatsby to improve load

--- src/components/ProjectRoll.js
+++ src/components/ProjectRoll.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link, graphql, StaticQuery } from 'gatsby'
+import Fade from 'react-reveal/Fade';
 import Media from 'react-media';
 
 class ProjectRoll extends React.Component {
@@ -12,62 +13,64 @@ class ProjectRoll extends React.Component {
       <section className="projectsList">
         {posts &&
           posts.map(({ node: post }, index) => (
-            <div className="fullWidth singleProject">
-              {index%2 === 0 && 
-                <Media queries={{ small: { maxWidth: 768 } }}>
+            <Fade>
+              <div className="fullWidth singleProject">
+                {index%2 === 0 && 
+                  <Media queries={{ small: { maxWidth: 768 } }}>
+                    {matches =>
+                      matches.small ? (
+                        <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(135deg, rgba(0,0,0,0.75) 33%, rgba(0,0,0,0.95) 55%),url("/img/${post.frontmatter.imageUrl})`}}>
+                          <div class="hideOnMobile"></div>
+                          <div className="perfectCenter oddProject">
+                            <h2>{post.frontmatter.title}</h2>
+                            <span>{post.frontmatter.category}</span>
+                            <p>{post.frontmatter.description}</p>
+                            <button className="button-white button-transparent"><span>More</span></button>
+                          </div>
+                        </Link>
+                      ) : (
+                        <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(135deg, rgba(0,0,0,0) 20%, rgba(0,0,0,0.95) 65%),url("/img/${post.frontmatter.imageUrl}")`}}>
+                          <div class="hideOnMobile"></div>
+                          <div className="perfectCenter oddProject">
+                            <h2>{post.frontmatter.title}</h2>
+                            <span>{post.frontmatter.category}</span>
+                            <p>{post.frontmatter.description}</p>
+                            <button className="button-white button-transparent"><span>More</span></button>
+                          </div>
+                        </Link>
+                      )
+                    }
+                  </Media>
+                }
+                {index%2 === 1 && 
+                  <Media queries={{ small: { maxWidth: 768 } }}>
                   {matches =>
                     matches.small ? (
-                      <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(135deg, rgba(0,0,0,0.75) 33%, rgba(0,0,0,0.95) 55%),url("/img/${post.frontmatter.imageUrl})`}}>
-                        <div class="hideOnMobile"></div>
-                        <div className="perfectCenter oddProject">
+                      <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(45deg, rgba(255,255,255,0.75) 45%, rgba(255,255,255,0.75) 66%),url("/img/${post.frontmatter.imageUrl}")`}}>
+                        <div className="perfectCenter evenProject">
                           <h2>{post.frontmatter.title}</h2>
-                          <span>{post.frontmatter.category}</span>
+                          <span>{post.frontmatter.category}</span> 
                           <p>{post.frontmatter.description}</p>
-                          <button className="button-white button-transparent"><span>More</span></button>
+                          <button className="button-black button-transparent"><span>More</span></button>
                         </div>
+                        <div class="hideOnMobile"></div>
                       </Link>
                     ) : (
-                      <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(135deg, rgba(0,0,0,0) 20%, rgba(0,0,0,0.95) 65%),url("/img/${post.frontmatter.imageUrl}")`}}>
-                        <div class="hideOnMobile"></div>
-                        <div className="perfectCenter oddProject">
+                      <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(45deg, rgba(255,255,255,0.75) 45%, rgba(0,0,0,0) 60%),url("/img/${post.frontmatter.imageUrl}")`}}>
+                        <div className="perfectCenter evenProject">
                           <h2>{post.frontmatter.title}</h2>
                           <span>{post.frontmatter.category}</span>
                           <p>{post.frontmatter.description}</p>
-                          <button className="button-white button-transparent"><span>More</span></button>
+                          <button className="button-black button-transparent"><span>More</span></button>
                         </div>
+                        <div class="hideOnMobile"></div>
                       </Link>
                     )
                   }
                 </Media>
-              }
-              {index%2 === 1 && 
-                <Media queries={{ small: { maxWidth: 768 } }}>
-                {matches =>
-                  matches.small ? (
-                    <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(45deg, rgba(255,255,255,0.75) 45%, rgba(255,255,255,0.75) 66%),url("/img/${post.frontmatter.imageUrl}")`}}>
-                      <div className="perfectCenter evenProject">
-                        <h2>{post.frontmatter.title}</h2>
-                        <span>{post.frontmatter.category}</span> 
-                        <p>{post.frontmatter.description}</p>
-                        <button className="button-black button-transparent"><span>More</span></button>
-                      </div>
-                      <div class="hideOnMobile"></div>
-                    </Link>
-                  ) : (
-                    <Link to={post.fields.slug} className="twoColumns fillContainer projectBG" style={{backgroundImage: `linear-gradient(45deg, rgba(255,255,255,0.75) 45%, rgba(0,0,0,0) 60%),url("/img/${post.frontmatter.imageUrl}")`}}>
-                      <div className="perfectCenter evenProject">
-                        <h2>{post.frontmatter.title}</h2>
-                        <span>{post.frontmatter.category}</span>
-                        <p>{post.frontmatter.description}</p>
-                        <button className="button-black button-transparent"><span>More</span></button>
-                      </div>
-                      <div class="hideOnMobile"></div>
-                    </Link>
-                  )
                 }
-              </Media>
-              }
-            </div>
+              </div>
+            </Fade>
           ))}
       </section>
     )
